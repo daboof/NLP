79e8e66bb3970c45c18ed2969aeb8062f348faf9
deba <debabrata1729@gmail.com>
2023-05-31 16:22:42 +0530

    changed the input of rouge function to original text unprocessed and summary

--- SummaryText.py
+++ SummaryText.py
@@ -8,6 +8,7 @@ import sklearn.feature_extraction
 from sklearn.feature_extraction.text import TfidfVectorizer
 import rouge
 from rouge import Rouge
+import re
 
 def calculate_tfidf_scores(texts):
     vectorizer = TfidfVectorizer()
@@ -47,7 +48,9 @@ def findkey(dictionary, value):
 
 text='''Incorporating cloud computing into heterogeneous networks, the heterogeneous cloud radio access network (H-CRAN) has been proposed as a promising paradigm to enhance both spectral and energy efficiencies. Developing interference suppression strategies is critical for suppressing the inter-tier interference between remote radio heads (RRHs) and a macro base station (MBS) in H-CRANs. In this paper, inter-tier interference suppression techniques are considered in the contexts of collaborative processing and cooperative radio resource allocation (CRRA). In particular, interference collaboration (IC) and beamforming (BF) are proposed to suppress the inter-tier interference, and their corresponding performance is evaluated. Closed-form expressions for the overall outage probabilities, system capacities, and average bit error rates under these two schemes are derived. Furthermore, IC- and BF-based CRRA optimization models are presented to maximize the RRH-accessed users' sum rates via power allocation, which is solved with convex optimization. Simulation results demonstrate that the derived expressions for these performance metrics for IC and BF are accurate, and the relative performance between IC and BF schemes depends on system parameters such as the number of antennas at the MBS, the number of RRHs, and the target signal-to-interference-plus-noise ratio threshold. Furthermore, it is seen that the sum rates of IC and BF schemes increase almost linearly with the transmit power threshold under the proposed CRRA optimization solution.
 '''
+texto=text
 text=text.lower()
+text=re.sub(r'[\(\)]','',text)
 text = ' '.join([word for word in text.split() if word not in (stopwords.words('english'))])
 
 text=text.lower()
@@ -67,9 +70,9 @@ for i in scoreset:
 summaryfinal=' '.join(summaryset)
 
 rouge=Rouge()
-scores=rouge.get_scores(text,summaryfinal)
+scores=rouge.get_scores(texto,summaryfinal)
 
-print("original abstract:  "+text+'\n')
+print("original abstract:  "+texto+'\n')
 print("summarized :  "+summaryfinal+'\n')
 print("rouge scores:  ")
 print(scores)
